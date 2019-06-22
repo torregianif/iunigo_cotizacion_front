10c0df39b25174dbbd143f406383a6143c613b55
Santiago <santimuller14@gmail.com>
2019-06-22 20:39:05 -0300

    sarasa

--- src/components/UserNameForm.js
+++ src/components/UserNameForm.js
@@ -8,102 +8,106 @@ import CardContent from '@material-ui/core/CardContent';
 
 
 const useStyles = makeStyles(theme => ({
-    container: {
-      display: 'flex',
-      flexWrap: 'wrap',
-    },
-    textField: {
-      marginLeft: theme.spacing(1),
-      marginRight: theme.spacing(1),
-      width: 500,
-    },
-    dense: {
-      marginTop: 19,
-    },
-    menu: {
-      width: 200,
-    },
-    logo:{
-      width: 10,  
-    },
-  }));
+  container: {
+    display: 'flex',
+    flexWrap: 'wrap',
+  },
+  textField: {
+    marginLeft: theme.spacing(1),
+    marginRight: theme.spacing(1),
+    width: 500,
+  },
+  dense: {
+    marginTop: 19,
+  },
+  menu: {
+    width: 200,
+  },
+  logo: {
+    width: 10,
+  },
+}));
 
 
 export default class UserNameForm extends Component {
 
-    continue = e => {
-        e.preventDefault();
-        this.props.nextStep();
-    }
-    
-    render() {
+  continue = e => {
+    e.preventDefault();
+    this.props.nextStep();
+  }
 
-        const classes = useStyles;
-        const { handleChange } = this.props
+  render() {
 
-        return (
+    const classes = useStyles;
+    const { handleChange } = this.props
 
-            <Container maxWidth="sm" className={classes.container}>
+    return (
 
-          
-              <Grid container justify="center" alignItems="center">
-                 <img src={logo} alt="logo" style={{
-                margin:10,width:200,height: 200,borderRadius: 100,
+      <Container maxWidth="sm" className={classes.container}>
 
-                 }}>
 
-                 </img>
+        <Grid container justify="center" alignItems="center">
+          <img src={logo} alt="logo" style={{
+            margin: 35, width: 160, borderRadius: 100,
+
+          }}>
+
+          </img>
+        </Grid>
+
+        <h2>Hola! Soy Jonathan. Tengo 30 años.</h2>
+        <p style={{ fontSize: "1.5rem" }}>Asesor Oficial de Iunigo!</p>
+        <h3>Contame sobre vos...</h3>
+        <form className={classes.form} noValidate>
+          <Grid container spacing={6}>
+            <Grid
+              container
+              direction="column"
+              justify="center"
+              alignItems="center"
+              item xs={12}>
+              <Grid style={{ width: "100%" }}>
+                <TextField
+                  style={{ width: "100%" }}
+                  id="name"
+                  label="Nombre"
+                  margin="normal"
+                  onChange={handleChange('name')}
+                />
+              </Grid>
+              <Grid style={{ width: "100%" }}>
+                <TextField
+                  style={{ width: "100%" }}
+                  id="lastname"
+                  label="Apellido"
+                  margin="normal"
+                  onChange={handleChange('lastName')}
+                />
+              </Grid>
+              <Grid style={{ width: "100%" }}>
+                <TextField
+                  style={{ width: "100%" }}
+                  id="standard-number"
+                  label="Edad"
+                  type="number"
+                  margin="normal"
+                  onChange={handleChange('age')}
+                />
               </Grid>
 
-            <h2>Hola! Soy Jonathan. Coticemos tú auto juntos.</h2>
-            <h3>Contame sobre vos</h3>
-                <form className={classes.form} noValidate>
-                    <Grid container spacing={6}>
-                        <Grid 
-                        container
-                        direction="column"
-                        justify="center"
-                        alignItems="center"
-                        item xs={12}>
-                    <Grid >
-                        <TextField 
-                            id="name"
-                            label="Nombre"
-                            margin="normal"
-                            onChange = {handleChange('name')}
-                            />
-                        </Grid>
-                    <Grid >
-                    <TextField
-                            id="lastname"
-                            label="Apellido"
-                            margin="normal"
-                            onChange = {handleChange('lastName')}
-                        />
-                        </Grid>
-                    <Grid >
-                    <TextField
-                            id="standard-number"
-                            label="Edad"
-                            type="number"
-                            margin="normal"
-                            onChange = {handleChange('age')}
-                        />
-                        </Grid>
-                     
-                       
-                        </Grid>
-                        <Grid item xs={12}>
-                            <Button variant="contained" color="primary" onClick={this.continue}>
-                                Continuar
+
+            </Grid>
+            <Grid item xs={12}>
+              <Button variant="contained" color="primary" onClick={this.continue}>
+                Continuar
                             </Button>
-                        </Grid>
-                    </Grid>
-                </form>
-                <div></div>
-           
-            </Container>
-                  
-        )
-    }
+            </Grid>
+          </Grid>
+        </form>
+        <div></div>
+
+      </Container>
+
+    )
+  }
 }
