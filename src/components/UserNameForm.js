b5b09750b6971627fea202906cb5e9ddd6490f8b
dev-AlanGaia <alan.j.gaia@gmail.com>
2019-06-22 21:32:39 -0300

    Soy alansuli

--- src/components/UserNameForm.js
+++ src/components/UserNameForm.js
@@ -2,9 +2,7 @@ import React, { Component } from 'react';
 import { Container, TextField, Button, Grid } from '@material-ui/core';
 import { makeStyles } from '@material-ui/core/styles';
 import logo from '../img/JonathanLeibiusky.jpg';
-import Card from '@material-ui/core/Card';
-import CardActions from '@material-ui/core/CardActions';
-import CardContent from '@material-ui/core/CardContent';
+
 
 
 const useStyles = makeStyles(theme => ({
